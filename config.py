5a79042703680e991aef04877274d211ff9210d9
Pawan Jain <42181691+pawangeek@users.noreply.github.com>
2019-11-28 13:41:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # twilio keys
-account_sid = 'AC86ee91e8491f5c9ced042224245b8821'
-auth_token = 'd88eac98af6ee2498c4e512fbfd7ef76'
+account_sid = '###############################'
+auth_token = '#################################'
 from_phone = '+12055288894'
